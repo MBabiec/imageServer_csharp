48f9e012b5b24ba5b1effc8bc4183922c5952974
MBabiec <mbabiec@amctech.com>
2025-01-15 15:13:21 +0100

    Works nice so far

---
+++ imageServer_csharp/Drive/DriveAccess.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageServer_csharp.Drive
+{
+    public class DriveAccess
+    {
+        private const string PathToServiceAccountKeyFile = @"Drive/serviceAccountCredentials.json";
+
+        private DriveService GetService()
+        {
+            string credJson = File.ReadAllText(PathToServiceAccountKeyFile);
+            GoogleCredential credential1 = GoogleCredential.FromJson(credJson);
+            GoogleCredential credential2 = credential1.CreateScoped(DriveService.ScopeConstants.DriveReadonly);
+            DriveService service = new(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential2
+            });
+            return service;
+        }
+
+        public async Task<byte[]?> GetFile(string fileId)
+        {
+            //return System.IO.File.ReadAllBytes("Drive/AnimeHoodies_1hfd6j1.jpeg");
+            try
+            {
+                var service = GetService();
+                MemoryStream ms = new();
+                var file = service.Files.Get(fileId);
+                var res = await file.DownloadAsync(ms);
+                if (res.Status.Equals(Google.Apis.Download.DownloadStatus.Completed))
+                {
+                    return ms.ToArray();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ imageServer_csharp/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using imageServer_csharp.Drive;
+using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+
+namespace imageServer_csharp;
+
+class Program
+{
+    private static TcpListener listener;
+    private static readonly string WebServerPath = @"Webserver";
+    private static List<string> imageIds;
+    private static Random rnd = new Random();
+
+    static void Main(string[] args)
+    {
+        using (StreamReader r = new StreamReader("server_conf.json"))
+        {
+            string json = r.ReadToEnd();
+            dynamic array = JsonConvert.DeserializeObject(json);
+            string host = array.hostname;
+            int port = array.port;
+            listener = new TcpListener(IPAddress.Parse(host), port);
+            Console.WriteLine($"Webserver started on {array.hostname}:{array.port}");
+            listener.Start();
+        }
+        Thread th = new Thread(new ThreadStart(StartListen));
+        th.Start();
+    }
+    private static void StartListen()
+    {
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            byte[] requestBytes = new byte[1024];
+            int bytesRead = stream.Read(requestBytes, 0, requestBytes.Length);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+            var requestHeaders = ParseHeaders(request);
+
+            string[] requestFirstLine = requestHeaders.requestType.Split(" ");
+            var requestedPath = requestFirstLine[1];
+            string httpVersion = requestFirstLine.LastOrDefault();
+            string contentType = requestHeaders.headers.GetValueOrDefault("Accept");
+            string contentEncoding = requestHeaders.headers.GetValueOrDefault("Acept-Encoding");
+            var fileContent = GetContent(requestedPath);
+            if (fileContent is not null)
+            {
+                SendHeaders(httpVersion, 200, "OK", requestedPath.Contains("image") ? "image" : $"text/{requestedPath.Substring(requestedPath.IndexOf(".") + 1)}", contentEncoding, 0, ref stream);
+                stream.Write(fileContent.Result);
+                sw.Stop();
+                Console.WriteLine($"Took {sw.ElapsedMilliseconds}");
+            }
+            else
+            {
+                SendHeaders(httpVersion, 404, "Page Not Found", contentType, contentEncoding, 0, ref stream);
+            }
+
+            if (requestFirstLine[0] != "GET")
+            {
+                SendHeaders(httpVersion, 405, "Method Not Allowed", contentType, contentEncoding, 0, ref stream);
+                //DUPA
+            }
+            client.Close();
+        }
+    }
+    private static (Dictionary<string, string> headers, string requestType) ParseHeaders(string headerString)
+    {
+        var headerLines = headerString.Split('\r', '\n');
+        string firstLine = headerLines[0];
+        var headerValues = new Dictionary<string, string>();
+        foreach (var headerLine in headerLines)
+        {
+            var headerDetail = headerLine.Trim();
+            var delimiterIndex = headerLine.IndexOf(':');
+            if (delimiterIndex >= 0)
+            {
+                var headerName = headerLine.Substring(0, delimiterIndex).Trim();
+                var headerValue = headerLine.Substring(delimiterIndex + 1).Trim();
+                headerValues.Add(headerName, headerValue);
+            }
+        }
+        return (headerValues, firstLine);
+    }
+    private static void SendHeaders(string? httpVersion, int statusCode, string statusMsg, string? contentType, string? contentEncoding, int byteLength, ref NetworkStream networkStream)
+    {
+        string responseHeaderBuffer = "";
+
+        responseHeaderBuffer = $"HTTP/1.1 {statusCode} {statusMsg}\r\n" +
+            $"Connection: Keep-Alive\r\n" +
+            $"Date: {DateTime.UtcNow.ToString()}\r\n" +
+            $"Content-Encoding: {contentEncoding}\r\n" +
+            $"Content-Type: {contentType}\r\n\r\n";
+
+        byte[] responseBytes = Encoding.UTF8.GetBytes(responseHeaderBuffer);
+        networkStream.Write(responseBytes, 0, responseBytes.Length);
+    }
+    private static async Task<byte[]?> GetContent(string requestedPath)
+    {
+        if (imageIds == null)
+        {
+            imageIds = [];
+            var path = @"Drive/data.csv";
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = ["#"];
+                csvParser.SetDelimiters([","]);
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                while (!csvParser.EndOfData)
+                {
+                    string[] fields = csvParser.ReadFields();
+                    imageIds.Add(fields[0]);
+                }
+            }
+        }
+        if (requestedPath == "/") requestedPath = "default.html";
+        string filePath = Path.Join(WebServerPath, requestedPath);
+        if (filePath.Contains("image"))
+        {
+            DriveAccess dr = new();
+            return await dr.GetFile(imageIds[rnd.Next(imageIds.Count)]);
+        }
+
+        if (!File.Exists(filePath)) return null;
+
+        else
+        {
+            byte[] file = System.IO.File.ReadAllBytes(filePath);
+            return file;
+        }
+    }
+}
